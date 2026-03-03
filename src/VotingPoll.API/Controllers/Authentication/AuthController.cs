13cf2cb42778272300777de2c67be64bff0db826
Tim VL <vanleemput.tim@gmail.com>
2026-03-03 08:52:36 +0100

    [Proper password validators]

--- src/VotingPoll.API/Controllers/Authentication/AuthController.cs
+++ src/VotingPoll.API/Controllers/Authentication/AuthController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.Data;
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
+using VotingPoll.Core.Exceptions.AuthExceptions;
 using VotingPoll.Core.Interfaces.Authentication;
 using VotingPoll.Core.Models.DTOs.Authentication;
+using VotingPoll.Core.Validation.AuthValidators;
 
 namespace VotingPoll.API.Controllers.Authentication;
 
@@ -10,15 +13,30 @@ namespace VotingPoll.API.Controllers.Authentication;
 public class AuthController : ControllerBase
 {
     private readonly IAuthService _authService;
+    private readonly ILogger<AuthController> _logger;
+    private readonly RegisterRequestPasswordValidator _registerRequestPasswordValidator;
 
-    public AuthController(IAuthService authService)
+    public AuthController(IAuthService authService, ILogger<AuthController> logger,
+        RegisterRequestPasswordValidator registerRequestPasswordValidator)
     {
         _authService = authService;
+        _logger = logger;
+        _registerRequestPasswordValidator = registerRequestPasswordValidator;
     }
 
     [HttpPost("register")]
     public async Task<ActionResult<AuthDto.AuthResponse>> RegisterAsync(RegisterRequest request)
     {
+        AuthDto.RegisterRequest registerRequestDto = new AuthDto.RegisterRequest
+        {
+            Email = request.Email,
+            Password = request.Password,
+        };
+
+        ValidationResult validationResult = await _registerRequestPasswordValidator.ValidateAsync(registerRequestDto);
+        if (!validationResult.IsValid)
+            return BadRequest(validationResult.Errors.Select(error => error.ErrorMessage));
+
         AuthDto.AuthResponse response = await _authService.RegisterAsync(request);
         return Ok(response);
     }
---
+++ src/VotingPoll.Core/Validation/AuthValidators/RegisterRequestPasswordValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using VotingPoll.Core.Models.DTOs.Authentication;
+
+namespace VotingPoll.Core.Validation.AuthValidators;
+
+public class RegisterRequestPasswordValidator : AbstractValidator<AuthDto.RegisterRequest>
+{
+    public RegisterRequestPasswordValidator()
+    {
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .Length(8, 100)
+            .WithMessage("Password must have at least 8 characters")
+            .Matches(@"\d")
+            .WithMessage("Password must contain at least one digit")
+            .Matches(@"[A-Z]")
+            .WithMessage("Password must contain at least one uppercase")
+            .Matches(@"[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character");
+    }
+}
